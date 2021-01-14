a94c4e32002f7a1e8fd02030e1275d8d8660e564
Sky Kruzer <skykrzr@gmail.com>
2021-01-14 13:37:48 +0200

    added sessions

--- server.js
+++ server.js
@@ -1,50 +1,57 @@
 // the server is written using ExpressJS framework
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const StopWatch = require('./stopwatch');
- const app = express();
- app.use(bodyParser.urlencoded({extended:false}));
- app.use(bodyParser.json());
+const { watch } = require('fs');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({ secret: 'start secret' }));
 
-//the lap shared by all the routes;
-const lap = {};
-
- //this API is used to add to the lap new stop watches
-app.post('/addWatch',(req,res) => {
-    console.log(req.query);
+const sessions = {};
+//this API is used to add to the lap new stop watches
+app.post('/addWatch', (req, res) => {
     let watchNum = req.query.name;
-    if(lap[watchNum] !== undefined) res.send('This watch is already exists')
+
+    if (sessions[req.sessionID] && (sessions[req.sessionID][watchNum] !== undefined)) res.send(`This watch is already exists`)
     else {
+        let lap = {};
         lap[watchNum] = new StopWatch(watchNum);
+        sessions[req.sessionID] = {
+            ...sessions[req.sessionID],
+            ...lap
+        }
+        console.log(sessions[req.sessionID]);
         res.send(`watch ${watchNum} was added to the lap`);
     }
 })
 
 // starting a selected stop watch
-app.post('/startWatch',(req,res) => {
+app.post('/startWatch', (req, res) => {
     let watchNum = req.query.name;
-    if(lap[watchNum] === undefined) res.send('the watch does not exist in the lap');
+    if (sessions[req.sessionID] && (sessions[req.sessionID][watchNum] === undefined)) res.send('the watch does not exist in the lap');
     else {
-        let watch = lap[watchNum];
+        let watch = sessions[req.sessionID][watchNum];
         watch.start();
-        res.send(`the watch ${watchNum} started tikking`);
+        res.send(`the watch ${watchNum} started tekking`);
     }
 })
 
 // stop the selected watch
-app.post('/stopWatch',(req,res) => {
+app.post('/stopWatch', (req, res) => {
     let watchNum = req.query.name;
-    if(lap[watchNum] === undefined) res.send('the watch does not exist in the lap');
+    if (sessions[req.sessionID][watchNum] === undefined) res.send('the watch does not exist in the lap');
     else {
-        let watch = lap[watchNum];
+        let watch =sessions[req.sessionID][watchNum];
         res.send(watch.stop());
     }
 })
 
 // show the laps 
-app.get('/showLap',(req,res) => {
-    res.send(lap);
+app.get('/showLap', (req, res) => {
+    res.send(sessions[req.sessionID]);
 })
 
 
- app.listen(3000);
+app.listen(3000);
--- stopwatch.js
+++ stopwatch.js
@@ -2,52 +2,31 @@
 // there are two main methods on the class
 // 1. startWatch - this method let the user start the watch
 // stopWatch - this method let the user stop the current watch
-
 class StopWatch {
-    constructor(watchNumber){
-       this.startTime = 0;
-       this.watchNumber = watchNumber;
-       this.stopFlag = true;
-       this.startFlag = false;
-    }
-    formatedTime = (time,elapsedTime) => {
-        console.log(elapsedTime)
-        switch (time){
-            case 'hours':
-                let hours = Math.floor((elapsedTime/(60*60))).toString();
-                if(hours.length === 1) hours = '0' + hours;
-                return hours;
-            case 'minutes':
-                let minutes = Math.floor((elapsedTime/60)).toString();
-                if(minutes.length === 1) minutes = '0' + minutes;
-                return minutes;
-            case 'seconds':
-                let seconds = elapsedTime.toString();
-                if(seconds.length === 1) seconds = '0' + seconds;
-                return seconds;
-        }
+    constructor(watchNumber) {
+        this.startTime = 0;
+        this.watchNumber = watchNumber;
+        this.stopFlag = true;
+        this.startFlag = false;
     }
     start = () => {
-        if(!this.startFlag){
+        if (!this.startFlag) {
             this.startTime = Date.now();
             this.stopFlag = false;
             this.startFlag = true;
-        } 
+        }
     }
-     stop = () => {
-         this.startFlag = false;
-         if(!this.stopFlag){
-            return (Date.now() - this.startTime).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0];
-/*             let elapsedTime =  Math.floor(Date.now()/1000 - this.startTime/1000);
-            let hours = this.formatedTime('hours',elapsedTime);
-            let minues = this.formatedTime('minutes',elapsedTime);
-            let seconds = this.formatedTime('seconds',elapsedTime);
-            this.stopFlag = true;
-            this.startTime = 0
-            return hours + ":" + minues + ":" + seconds */
-         } else {
-             return 'the watch already stopped'
-         }
+    stop = () => {
+        this.startFlag = false;
+        if (!this.stopFlag) {
+            this.stop = true;
+            let elapsedTime = Math.floor(Date.now() - this.startTime);
+            let formatted = new Date(elapsedTime).toISOString().substr(11,8);
+            return formatted;
+
+        } else {
+            return 'the watch already stopped'
+        }
     }
 }
 
